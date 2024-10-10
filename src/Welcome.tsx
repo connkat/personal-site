ec0e0eef40ff0a0ab776444489fd29502ec6a596
Kat <hi.connkat@gmail.com>
2024-10-09 22:20:14 -0600

    landing page

---
+++ src/Welcome.tsx
@@ -0,0 +1,17 @@
+import "./welcome.css";
+import "animate.css";
+
+import Logo from "./assets/logo-rectangle.png";
+
+function Welcome() {
+	return (
+		<div className="Welcome">
+			<img src={Logo} alt="logo" className="logo" />
+			<h1 className="animate__animated animate__fadeInUp">
+				Fullstack Development, handled.
+			</h1>
+		</div>
+	);
+}
+
+export default Welcome;
