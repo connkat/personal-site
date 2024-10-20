e4277a5b4e7f89e2b52a183b8c5138d6a0001fb7
Kat <hi.connkat@gmail.com>
2024-10-20 01:04:05 -0600

    added links

--- src/pages/Nineties.tsx
+++ src/pages/Nineties.tsx
@@ -10,12 +10,96 @@ function Nineties() {
 					<h4>
 						<a href="#home">Home</a> <span className="line">|</span>
 						<a href="#about">About Me</a> <span className="line">|</span>
+						<a href="#links">Links</a> <span className="line">|</span>
 						<a href="#projects">Projects</a> <span className="line">|</span>
 					</h4>
 				</div>
-				<h2 id="about">About Me:</h2>
-
-				<h2 id="projects">Projects I am working on:</h2>
+				<div id="about">
+					<h2>About Me:</h2>
+					<p>I am passionate about web design!</p>
+				</div>
+				<h2>Links:</h2>
+				<div id="links">
+					<div className="single-link">
+						<a
+							href="https://github.com/connkat/Resume/blob/master/KConnolly.pdf"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								src="https://win98icons.alexmeub.com/icons/png/write_wordpad-1.png"
+								alt="resume"
+								className="icon-tiny"
+							/>
+							<p>Resume</p>
+						</a>
+					</div>
+					<div className="single-link">
+						<a
+							href="https://linkedin.com/in/connkat"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								src={
+									"https://win98icons.alexmeub.com/icons/png/world_phonereceiver.png"
+								}
+								alt="linkedIn"
+								className="icon-tiny-linkedin"
+							/>
+							<p>LinkedIn</p>
+						</a>
+					</div>
+					<div className="single-link">
+						<a
+							href="https://www.github.com/connkat"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								src={
+									"https://win98icons.alexmeub.com/icons/png/computer_explorer-4.png"
+								}
+								alt="github"
+								className="icon-tiny"
+							/>
+							<p>Github</p>
+						</a>
+					</div>
+					<div className="single-link">
+						<a
+							href="https://connkat.medium.com/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								src={"https://win98icons.alexmeub.com/icons/png/printer-0.png"}
+								alt="Medium"
+								className="icon-tiny"
+							/>
+							<p>Medium</p>
+						</a>
+					</div>
+				</div>
+				<div id="projects">
+					<h2>Projects</h2>
+					<h3>
+						I could go on about my professional experience with: <br />
+						<a href="http://neofinancial.com" target="_blank" rel="noreferrer">
+							Neo Financial
+						</a>
+						,{" "}
+						<a href="http://lighthouselabs.ca" target="_blank" rel="noreferrer">
+							Lighthouse Labs
+						</a>
+						,{" "}
+						<a href="http://checkfront.com" target="_blank" rel="noreferrer">
+							Checkfront
+						</a>
+						, or my Freelance work.{" "}
+					</h3>
+					<h3>Instead here are some of my fun personal projects:</h3>
+				</div>
 			</div>
 			<footer className="footer">
 				<p>Thanks for coming!</p>
