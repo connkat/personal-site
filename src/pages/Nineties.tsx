d92e533d2da891abd42fa4e71d64f63f6a2e7918
Kat <hi.connkat@gmail.com>
2024-10-20 00:33:11 -0600

    cleaned up 90s

--- src/pages/Nineties.tsx
+++ src/pages/Nineties.tsx
@@ -3,23 +3,23 @@ import "../styling/nineties.css";
 function Nineties() {
 	return (
 		<div className="Nineties">
-			<h1 className="tab blink">Welcome to my website!</h1>
-			<img src="https://i.imgur.com/i6hvJjx.gif" alt="rainbow-divider" />
-			<div id="links">
-				<h4>
-					<a href="#home">Home</a> <span className="line">|</span>
-					<a href="#about">About Me</a> <span className="line">|</span>
-					<a href="#working">Projects I am Working On</a>{" "}
-					<span className="line">|</span>
-					<a href="#made">Stuff I Made</a>
-				</h4>
+			<div className="content">
+				<h1 className="tab blink">Welcome to my website!</h1>
+				<img src="https://i.imgur.com/i6hvJjx.gif" alt="rainbow-divider" />
+				<div id="links">
+					<h4>
+						<a href="#home">Home</a> <span className="line">|</span>
+						<a href="#about">About Me</a> <span className="line">|</span>
+						<a href="#projects">Projects</a> <span className="line">|</span>
+					</h4>
+				</div>
+				<h2 id="about">About Me:</h2>
+
+				<h2 id="projects">Projects I am working on:</h2>
 			</div>
-			<h2>This is the base file for the 90s themed website I want to make</h2>
-			<h2 id="working">Projects I am working on</h2>
-			<footer id="footer">
-				<span>
-					<img src="https://i.imgur.com/Q	kBkUkC.gif" alt="ie-logo" />
-				</span>
+			<footer className="footer">
+				<p>Thanks for coming!</p>
+				<img src="https://i.imgur.com/Q	kBkUkC.gif" alt="ie-logo" />
 			</footer>
 		</div>
 	);
